13ca336c433c214a1fcd27fab050d7215fe7aec7
Lilo Onwuzu <lpr422@gmail.com>
2016-05-15 23:56:18 -0700

    Added some front-end features

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,147 +1,112 @@
-// import static spark.Spark.*;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import java.util.List;
-// import java.util.HashMap;
-//
-// public class App {
-//
-//   public static void main(String [] args) {
-//     staticFileLocation("/public");
-//     String layout = "templates/layout.vtl";
-//
-//     get("/stylists", (request, response) -> {
-//       HashMap model = new HashMap();
-//       model.put("stylists", Stylist.all());
-//       model.put("template", "templates/stylists.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     post("/stylists", (request, response) -> {
-//       HashMap model = new HashMap();
-//       String newInputStylist = request.queryParams("inputStylist");
-//       Stylist stylist = new Stylist(newInputStylist);
-//       // create stylist: save to the DB as a row in the sylists table
-//       stylist.save();
-//       model.put("stylists", Stylist.all());
-//       model.put("template", "templates/stylists.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     get("/stylists/new", (request, response) -> {
-//       HashMap model = new HashMap();
-//       model.put("template", "templates/newStylist.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     get("/stylists/:stylist_id", (request, response) -> {
-//       HashMap<String, Object> model = new HashMap<String, Object>();
-//       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
-//       model.put("stylist", stylist);
-//       model.put("clients", stylist.getClients());
-//       model.put("template", "templates/stylist.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     // renders edit Stylist form
-//     get("/stylists/:stylist_id/edit", (request, response) -> {
-//       HashMap model = new HashMap();
-//       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
-//       model.put("stylist", stylist);
-//       model.put("template", "templates/editStylist.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     // post to this url path whenever a stylist name is edited
-//     post("/stylists/:stylist_id", (request, response) -> {
-//       HashMap<String, Object> model = new HashMap<String, Object>();
-//       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
-//       String name = request.queryParams("editStylist");
-//       // update stylist
-//       stylist.update(name);
-//       response.redirect("/stylists/" + stylist.getId());
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     // deletes stylist after you click on the link and then redirects to full list of stylists page
-//     get("/stylists/:stylist_id/remove", (request, response) -> {
-//       HashMap model = new HashMap();
-//       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
-//       // delete stylist
-//       stylist.delete();
-//       response.redirect("/stylists");
-//       model.put("stylist", stylist);
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     // renders add client form after you click on the "addClient" link
-//     get("/stylists/:stylist_id/clients/new", (request, response) -> {
-//       HashMap model = new HashMap();
-//       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
-//       model.put("stylist", stylist);
-//       model.put("template", "templates/newClient.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     get("/stylists/:stylist_id/clients", (request, response) -> {
-//       HashMap model = new HashMap();
-//       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
-//       model.put("stylist", stylist);
-//       model.put("clients", stylist.getClients());
-//       model.put("template", "templates/clients.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     // post to this url path whenever a new client is created
-//     post("/stylists/:stylist_id/clients", (request, response) -> {
-//       HashMap<String, Object> model = new HashMap<String, Object>();
-//       // read stylist
-//       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
-//       String newInputClient = request.queryParams("inputClient");
-//       // create a new client. Also a new one to many association is made between this client and this stylist
-//       Client client = new Client(newInputClient, stylist.getId());
-//       client.save();
-//       model.put("stylist", stylist);
-//       model.put("clients", stylist.getClients());
-//       model.put("template", "templates/clients.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     // brings up page that allows you to delete or update this client
-//     get("/stylists/:stylist_id/clients/:id", (request, response) -> {
-//       HashMap model = new HashMap();
-//       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
-//       Client client = Client.find(Integer.parseInt(request.params(":id")));
-//       model.put("stylist", stylist);
-//       model.put("client", client);
-//       model.put("template", "templates/client.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     // updates client
-//     post("/stylists/:stylist_id/clients/:id", (request, response) -> {
-//       HashMap model = new HashMap();
-//       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
-//       Client client = Client.find(Integer.parseInt(request.params(":id")));
-//       String editClient = request.queryParams("editClient");
-//       client.update(editClient);
-//       client = Client.find(Integer.parseInt(request.params(":id")));
-//       model.put("stylist", stylist);
-//       model.put("client", client);
-//       model.put("template", "templates/client.vtl");
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//     // deletes client after you click on the link and then redirects to full list of clients page
-//     get("/stylists/:stylist_id/clients/:id/remove", (request, response) -> {
-//       HashMap model = new HashMap();
-//       Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
-//       Client client = Client.find(Integer.parseInt(request.params(":id")));
-//       client.delete();
-//       response.redirect("/stylists/" + stylist.getId() + "/clients");
-//       model.put("stylist", stylist);
-//       return new ModelAndView(model, layout);
-//     }, new VelocityTemplateEngine());
-//
-//   }
-// }
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import java.util.List;
+import java.util.HashMap;
+
+public class App {
+
+  public static void main(String [] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // new Band form
+    get("/bands/new", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/newBand.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // new Venue form
+    get("/venues/new", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/newVenue.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // all bands page
+    get("/bands", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("bands", Band.all());
+      model.put("template", "templates/bands.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // all venues page
+    get("/venues", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("venues", Venue.all());
+      model.put("template", "templates/venues.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // post new band
+    post("/bands", (request, response) -> {
+      HashMap model = new HashMap();
+      String newInputBand = request.queryParams("inputBand");
+      Band band = new Band(newInputBand);
+      band.save();
+      model.put("bands", Band.all());
+      model.put("template", "templates/bands.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // post new venue
+    post("/venues", (request, response) -> {
+      HashMap model = new HashMap();
+      String newInputVenue = request.queryParams("inputVenue");
+      Venue venue = new Venue(newInputVenue);
+      venue.save();
+      model.put("venues", Venue.all());
+      model.put("template", "templates/venues.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // band details
+    get("/bands/:band_id", (request, response) -> {
+      HashMap model = new HashMap();
+      Band band = Band.find(Integer.parseInt(request.params(":band_id")));
+      model.put("band", band);
+      model.put("venues", band.getVenues());
+      model.put("template", "templates/band.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // venue details
+    get("/venues/:venue_id", (request, response) -> {
+      HashMap model = new HashMap();
+      Venue venue = Venue.find(Integer.parseInt(request.params(":venue_id")));
+      model.put("venue", venue);
+      model.put("bands", venue.getBands());
+      model.put("template", "templates/venue.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // add venue form
+    get("/bands/:band_id/add_venue", (request, response) -> {
+      HashMap model = new HashMap();
+      Band band = Band.find(Integer.parseInt(request.params(":band_id")));
+      model.put("band", band);
+      model.put("allVenues", Venue.all());
+      model.put("template", "templates/addVenue.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // add band form
+    get("/venues/:venue_id/add_band", (request, response) -> {
+      HashMap model = new HashMap();
+      Venue venue = Venue.find(Integer.parseInt(request.params(":venue_id")));
+      model.put("venue", venue);
+      model.put("allBands", Band.all());
+      model.put("template", "templates/addBand.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  }
+}
