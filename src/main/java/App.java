2827c56637c517c7592833ccb8e273f1cd7a9b94
Lilo Onwuzu <lpr422@gmail.com>
2017-05-02 16:01:58 -0700

    deploy

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,17 @@ import java.util.HashMap;
 public class App {
 
   public static void main(String [] args) {
+    // set up for heroku
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
+
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
